7631b56c8069902ad6ea3ce8d0f218d86f79c74f
Rodrigo <rodrigolp0521@gmail.com>
2025-01-26 10:34:51 -0600

    feat: add docker-compose & seeding

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,13 +1,19 @@
+import { relations } from "drizzle-orm";
 import {
+  doublePrecision,
   integer,
   pgTable,
   serial,
+  text,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
 
 export const roomingListBookings = pgTable("DRL_Rooming_List_Bookings", {
   id: serial("id").primaryKey(),
+  rooming_list_id: integer("rooming_list_id")
+    .references(() => roomingList.id)
+    .notNull(),
   hotel_confirmation_code: varchar("hotel_confirmation_code"),
   booking_external_id: serial("booking_external_id"),
   primary_guest: varchar("primary_guest"),
@@ -23,3 +29,112 @@ export const roomingListBookings = pgTable("DRL_Rooming_List_Bookings", {
   crewfare_confirmation_code: varchar("crewfare_confirmation_code"),
   room_external_id: varchar("room_external_id"),
 });
+
+export const roomingList = pgTable("DRL_Rooming_List", {
+  id: serial("id").primaryKey(),
+  rfp_id: integer("rfp_id")
+    .notNull()
+    .references(() => rfp.id)
+    .notNull(),
+  status_id: integer("status_id")
+    .references(() => status.id)
+    .notNull(),
+  platform_id: integer("platform_id")
+    .references(() => platform.id)
+    .notNull(),
+  hotel_id: integer("hotel_id")
+    .references(() => hotel.id)
+    .notNull(),
+  cutoff_date: timestamp("cutoff_date").notNull(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  modified_at: timestamp("modified_at")
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
+
+export const roomingListRelations = relations(roomingList, ({ many, one }) => ({
+  rooming_list_bookings: many(roomingListBookings),
+  rfp: one(rfp, {
+    fields: [roomingList.rfp_id],
+    references: [rfp.rfp_launchpad_id],
+  }),
+  status: one(status, {
+    fields: [roomingList.status_id],
+    references: [status.id],
+  }),
+  platform: one(platform, {
+    fields: [roomingList.platform_id],
+    references: [platform.id],
+  }),
+  hotel: one(hotel, {
+    fields: [roomingList.hotel_id],
+    references: [hotel.id],
+  }),
+}));
+
+export const bookingsRelations = relations(roomingListBookings, ({ one }) => ({
+  rooming_list: one(roomingList, {
+    fields: [roomingListBookings.rooming_list_id],
+    references: [roomingList.id],
+  }),
+}));
+
+export const hotel = pgTable("DRL_Hotel", {
+  id: serial("id").primaryKey(),
+  connect_id: integer("connect_id").unique(),
+  name: text("name"),
+  chain: varchar("chain"),
+  zipcode: integer("zipcode"),
+  lng: doublePrecision("lng"),
+});
+
+export const hotelRelations = relations(hotel, ({ many }) => ({
+  roomingList: many(roomingList),
+  user: many(user),
+}));
+
+export const status = pgTable("DRL_Status", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+});
+
+export const statusRelations = relations(status, ({ many }) => ({
+  roomingList: many(roomingList),
+}));
+
+export const platform = pgTable("DRL_Platform", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+});
+
+export const platformRelations = relations(platform, ({ many }) => ({
+  roomingList: many(roomingList),
+}));
+
+export const user = pgTable("DRL_user", {
+  id: serial("id").primaryKey(),
+  hotel_id: integer("hotel_id").references(() => hotel.id),
+  email: varchar("email"),
+});
+
+export const userRelations = relations(user, ({ one }) => ({
+  hotel: one(hotel, {
+    fields: [user.hotel_id],
+    references: [hotel.id],
+  }),
+}));
+
+export const rfp = pgTable("DRL_Rfp", {
+  id: serial("id").primaryKey(),
+  rfp_launchpad_id: varchar("rfp_launchpad_id").unique(),
+  event_name: text("event_name").notNull(),
+  event_internal_name: text("event_internal_name").notNull(),
+  event_start_date: timestamp("event_start_date").notNull(),
+  event_end_date: timestamp("event_end_date").notNull(),
+  agreement_type: text("agreement_type").notNull(),
+  agreement_path: varchar("agreement_path").notNull(),
+});
+
+export const rfpRelations = relations(rfp, ({ many }) => ({
+  roomingList: many(roomingList),
+}));
---
+++ drizzle/seeding/index.ts
@@ -0,0 +1,111 @@
+import { db } from "../db";
+import {
+  roomingList,
+  roomingListBookings,
+  rfp,
+  hotel,
+  platform,
+  status,
+} from "../schema";
+import test_data from "./test-data.json";
+
+import { eq } from "drizzle-orm";
+
+const seedData = async (jsonData) => {
+  try {
+    const statusData = await db.select().from(status).where(eq(status.id, 1));
+
+    if (statusData.length === 0) {
+      await db.insert(status).values([
+        { id: 1, name: "received" },
+        { id: 2, name: "completed" },
+        { id: 3, name: "archived" },
+        { id: 4, name: "updated" },
+      ]);
+    }
+
+    for (const data of jsonData) {
+      const rfpData = data.drl_rfp;
+      const rfpRecord = await db
+        .select()
+        .from(rfp)
+        .where(eq(rfp.rfp_launchpad_id, rfpData.rfp_launchpad_id));
+      if (rfpRecord.length === 0) {
+        await db.insert(rfp).values({
+          id: data.rfp_id,
+          rfp_launchpad_id: rfpData.rfp_launchpad_id,
+          event_name: rfpData.event_name,
+          event_internal_name: rfpData.event_internal_name,
+          event_start_date: new Date(rfpData.event_start_date),
+          event_end_date: new Date(rfpData.event_end_date),
+          agreement_type: rfpData.agreement_type,
+          agreement_path: rfpData.agreement_path,
+        });
+      }
+
+      const hotelRecord = await db
+        .select()
+        .from(hotel)
+        .where(eq(hotel.id, data.hotel_id));
+      if (hotelRecord.length === 0) {
+        await db.insert(hotel).values({
+          id: data.hotel_id,
+          name: `Hotel ${data.hotel_id}`,
+        });
+      }
+
+      const platformRecord = await db
+        .select()
+        .from(platform)
+        .where(eq(platform.id, data.platform_id));
+      if (platformRecord.length === 0) {
+        await db.insert(platform).values({
+          id: data.platform_id,
+          name: `Platform ${data.platform_id}`,
+        });
+      }
+
+      const roomingListId = await db
+        .insert(roomingList)
+        .values({
+          id: data.id,
+          rfp_id: data.rfp_id,
+          status_id: data.status_id,
+          platform_id: data.platform_id,
+          hotel_id: data.hotel_id,
+          cutoff_date: new Date(data.cutoff_date),
+          created_at: new Date(data.created_at),
+          modified_at: new Date(data.modified_at),
+        })
+        .returning({ id: roomingList.id });
+
+      for (const booking of data.drl_rooming_list_bookings) {
+        const checkIn = new Date(booking.check_in);
+        const checkOut = new Date(booking.check_out);
+        const totalNights = Math.ceil(
+          (checkOut.getTime() - checkIn.getTime()) / (1000 * 60 * 60 * 24)
+        );
+
+        await db.insert(roomingListBookings).values({
+          rooming_list_id: roomingListId[0].id,
+          primary_guest: booking.primary_guest,
+          roommates: booking.roommates,
+          guests: booking.guests,
+          room_name: booking.room_name,
+          check_in: checkIn,
+          check_out: checkOut,
+          total_nights: totalNights,
+          room_external_id: booking.room_code,
+        });
+      }
+    }
+
+    console.log("Data seeded successfully");
+  } catch (err: unknown) {
+    throw new Error(`seed already executed: ${err} `);
+  }
+};
+
+(async () => {
+  await seedData(test_data);
+})();
