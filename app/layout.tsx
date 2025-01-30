3dc96e7eebc51b18487d38965dc4afb32b42b5dc
Rodrigo <rodrigolp0521@gmail.com>
2025-01-29 22:20:47 -0600

    fix: lint issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
+  subsets: ["latin"],
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
