ea96eee71b95e69bbd2c7eed977e95385c84d98b
Rodrigo <rodrigolp0521@gmail.com>
2025-02-15 20:16:32 -0600

    fix: build issues

--- apps/web/components/organisms/search-bar/index.tsx
+++ apps/web/components/organisms/search-bar/index.tsx
@@ -6,30 +6,6 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import styles from "./styles.module.css";
 import { SearchIcon } from "./search-icon";
 
-function parseQuery(input: string) {
-  const datePattern =
-    /(?:before:(\d{4}(?:[-/]\d{2})?(?:[-/]\d{2})?))|(?:after:(\d{4}(?:[-/]\d{2})?(?:[-/]\d{2})?))/g;
-  const dateRange: { before: string | null; after: string | null } = {
-    before: null,
-    after: null,
-  };
-
-  for (const match of input.matchAll(datePattern)) {
-    const beforeDate = match[1] ? validateDate(match[1]) : null;
-    const afterDate = match[2] ? validateDate(match[2]) : null;
-
-    if (beforeDate) dateRange.before = beforeDate;
-    if (afterDate) dateRange.after = afterDate;
-  }
-
-  const cleanedQuery = input
-    .replace(datePattern, "")
-    .replace(/\s+/g, " ")
-    .trim();
-
-  return { query: cleanedQuery, ...dateRange };
-}
-
 export const SearchBar = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -42,23 +18,13 @@ export const SearchBar = () => {
       .split(",")
       .map((filter) => +filter) || [];
 
-  function handleSearch(term: string) {
+  function handleSearch(query: string) {
     const params = new URLSearchParams(searchParams);
 
-    if (term) {
-      const { query, after, before } = parseQuery(term);
-
+    if (query) {
       params.set("query", query);
-
-      if (after) params.set("after", after);
-      else params.delete("after");
-
-      if (before) params.set("before", before);
-      else params.delete("before");
     } else {
       params.delete("query");
-      params.delete("before");
-      params.delete("after");
     }
     replace(`${pathname}?${params.toString()}`);
   }
